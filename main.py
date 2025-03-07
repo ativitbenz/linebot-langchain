8df3a6de92eefa40a0c18f7f4ef00d6e9516dcf5
benzativit <57803622+ativitbenz@users.noreply.github.com>
2025-03-07 14:38:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ line_bot_api = AsyncLineBotApi(channel_access_token, async_http_client)
 parser = WebhookParser(channel_secret)
 
 # Langchain (you must use 0613 model to use OpenAI functions.)
-model = ChatOpenAI(model="gpt-3.5-turbo-0613")
+model = ChatOpenAI(model="gpt-4o-mini")
 tools = [StockPriceTool(), StockPercentageChangeTool(),
          StockGetBestPerformingTool()]
 open_ai_agent = initialize_agent(tools,
